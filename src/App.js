45154b8259759422088132580e866ccaf5ad81de
Emsun <emsunconcept@gmail.com>
2023-02-01 01:55:33 +0100

    Fixed the error

--- src/App.js
+++ src/App.js
@@ -1,43 +1,67 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import FetchPayments from './components/FetchPayments'
-import MakePayment from './components/MakePayment/MakePayment'
-import axios from 'axios'
-
-
+import React from "react";
+import { useState, useEffect } from "react";
+// import FetchPayments from "./components/FetchPayments";
+import MakePayment from "./components/MakePayment/MakePayment";
+import axios from "axios";
 
 const App = () => {
-const [transactionData, setTransactionData] = useState([])
-const [show, setShow] = useState(true)
+	const [transactionData, setTransactionData] = useState([]);
+	const [isloading, setisloading] = useState(false);
 
-  const url = "https://api.paystack.co/transaction"
+	const url = "https://api.paystack.co/transaction";
 
-    const fetchPay = async () => {
-        const response = await axios.get(url, {
-          headers: { 'Content-Type': 'application/json',
-           "Authorization": `Bearer ${process.env.REACT_APP_SECRET_KEY}`},
-        })
-        const dataResponse = response.data.data
-        console.log(dataResponse)
-        return dataResponse
-        setTransactionData(dataResponse)
-        // console.log(transactionData)
-    }
-    
-    // useEffect(() => {
-    //   fetchPay()
-    // }, [])
-  
-  
+	useEffect(() => {
+		const fetchPay = async () => {
+			setisloading(true);
+			try {
+				const response = await axios.get(url, {
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${process.env.REACT_APP_SECRET_KEY}`,
+					},
+				});
+				const dataResponse = response.data.data;
+				console.log(dataResponse);
+				setTransactionData(dataResponse);
+			} catch (error) {
+				console.log(error);
+			}
+			setisloading(false);
+			console.log("Data fetch complete");
+		};
+		fetchPay();
+	}, []);
 
-  return (
-    <>
-    <MakePayment/>
-    {/* <FetchPayments transactionData = {transactionData} setTransactionData={setTransactionData} /> */}
-    <button onClick={()=> fetchPay()}> fetch Transactions</button>
-    <button onClick={()=> setShow(!show)}> set show</button>
-    {show ? "show" : "notShow"}
-    {transactionData.length = 0 ? (<button onClick={()=> fetchPay()}> fetch Transactions</button>) : (transactionData.map((transaction, index) => {
+	return (
+		<>
+			<MakePayment />
+
+			{isloading ? (
+				<div>Loading...</div>
+			) : (
+				transactionData.map((transaction, index) => {
+					const {
+						amount,
+						customer: { first_name, last_name, email },
+						status,
+						reference,
+					} = transaction;
+					return (
+						<ul key={index}>
+							<li>First Name: {first_name}</li>
+							<li>Last Name: {last_name}</li>
+							<li>Email: {email}</li>
+							<li>Amount: {amount}</li>
+							<li>Status: {status}</li>
+							<li>Refrence: {reference}</li>
+						</ul>
+					);
+				})
+			)}
+
+			{/* <FetchPayments transactionData = {transactionData} setTransactionData={setTransactionData} /> */}
+
+			{/* {transactionData.length = 0 ? (<button onClick={()=> fetchPay()}> fetch Transactions</button>) : (transactionData.map((transaction, index) => {
       const {amount, customer: {first_name, last_name, email}, status, reference } = transaction
     
       return(
@@ -52,9 +76,9 @@ const [show, setShow] = useState(true)
       )
       
     })) }
-  
-    </>
-  )
-}
+   */}
+		</>
+	);
+};
 
-export default App
+export default App;
